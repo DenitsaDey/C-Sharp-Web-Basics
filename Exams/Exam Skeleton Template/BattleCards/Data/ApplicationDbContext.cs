871a2efad4c29f0df242150e3f0d23c8099d5ee7
Deni Dey <denitsa.dey@gmail.com>
2021-05-19 05:31:50 +0300

    Add files via upload

---
+++ Exams/Exam Skeleton Template/BattleCards/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿namespace SulsApp.Data
+{
+    using Microsoft.EntityFrameworkCore;
+
+    public class ApplicationDbContext : DbContext
+    { 
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+            optionsBuilder.UseSqlServer(DatabaseConfiguration.ConnectionString);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
