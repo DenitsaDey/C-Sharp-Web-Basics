0679bac04201a44671bde6c93fc7bc1b4b021505
Denitsa <denitsa.dey@gmail.com>
2021-05-30 07:47:09 +0300

    Initial HttpServerSetUp

---
+++ 04. Workshop/MyWebServer/MyWebServer.Server/Http/HttpRequest.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyWebServer.Server.Http
+{
+    public class HttpRequest
+    {
+        private const string NewLine = "\r\n";
+        public HttpRequestMethod Method { get; private set; }
+
+        public string Url { get; private set; }
+
+        public HttpHeaderCollection Headers{ get; private set; }
+
+        public string Body { get; private set; }
+
+        public static HttpRequest Parse(string request)
+        {
+            var lines = request.Split(NewLine);
+
+            var startLine = lines.First().Split(" ");
+
+            var method = ParseHttpRequestMethod(startLine[0]);
+            var url = startLine[1];
+
+            var headers = ParseHttpHeaders(lines.Skip(1));
+
+            var bodyLines = lines.Skip(headers.Count + 2).ToArray();
+            var body = string.Join(NewLine, bodyLines);
+
+            return new HttpRequest
+            {
+                Method = method,
+                Url = url,
+                Headers = headers,
+                Body = body
+            };
+        }
+
+
+        private static HttpRequestMethod ParseHttpRequestMethod(string method)
+        {
+            return method.ToUpper() switch
+            {
+                "GET" => HttpRequestMethod.Get,
+                "POST" => HttpRequestMethod.Post,
+                "PUT" => HttpRequestMethod.Put,
+                "DELETE" => HttpRequestMethod.Delete,
+                _ => throw new InvalidOperationException($"Method '{method}' is not supported."),
+            };
+        }
+
+        private static HttpHeaderCollection ParseHttpHeaders(IEnumerable<string> headerLines)
+        {
+            var headerCollection = new HttpHeaderCollection();
+
+            foreach (var headerLine in headerLines)
+            {
+                if (headerLine == string.Empty)
+                {
+                    break;
+                }
+
+                var headerParts = headerLine.Split(":", 2);
+
+                if(headerParts < 0)
+                {
+                    throw new InvalidOperationException("Request is not valid.");
+                }
+                var header = new HttpHeader
+                {
+                    Name = headerLine.Substring(0, headerParts),
+                    Value = headerLine[(headerParts + 1)..].Trim()
+                };
+
+                headerCollection.Add(header);
+            }
+
+            return headerCollection;
+        }
+        //private static string[] GetStartLine(string request)
+        //{
+
+        //}
+    }
+}
---
+++ 04. Workshop/MyWebServer/MyWebServer.Server/HttpServer.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyWebServer.Server
+{
+    public class HttpServer
+    {
+        private readonly IPAddress ipAddress;
+        private readonly int port;
+        private readonly TcpListener listener;
+
+        public HttpServer(string ipAddress, int port)
+        {
+            this.ipAddress = IPAddress.Parse(ipAddress);
+            this.port = port;
+            this.listener = new TcpListener(this.ipAddress, port);
+        }
+
+        public async Task Start()
+        {
+            this.listener.Start();
+
+            Console.WriteLine($"Server started on port {port}...");
+            while (true)
+            {
+                var connection = await this.listener.AcceptTcpClientAsync();
+
+                var networkStream = connection.GetStream();
+
+                var request = await this.ReadRequest(networkStream);
+
+                Console.WriteLine(request);
+
+                await WriteResponse(networkStream);
+
+                connection.Close();
+            }
+        }
+
+        private async Task<string> ReadRequest(NetworkStream networkStream)
+        {
+
+            var bufferLength = 1024;
+            var buffer = new byte[bufferLength];
+
+            var requestBuilder = new StringBuilder();
+
+            while (networkStream.DataAvailable)
+            {
+                var bytesRead = await networkStream.ReadAsync(buffer, 0, bufferLength);
+
+                requestBuilder.Append(Encoding.UTF8.GetString(buffer, 0, bytesRead));
+            }
+
+            return requestBuilder.ToString();
+        }
+
+        private async Task WriteResponse(NetworkStream networkStream)
+        {
+            var content = "<h1>Здравейте от Дени!</h1>";
+            var contentLength = Encoding.UTF8.GetByteCount(content);
+
+            var response = $@"HTTP/1.1 200 OK
+Content-Length: {contentLength}
+content-type: text/html; charset=UTF-8
+
+{content}";
+
+            var responseBytes = Encoding.UTF8.GetBytes(response);
+
+            await networkStream.WriteAsync(responseBytes);
+        }
+    }
+}
